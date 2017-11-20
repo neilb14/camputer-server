6b7da8da3793514539742dfea5f18fdca4ba53d4
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-11-19 21:34:30 -0700

    use Development Config in manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from camputer import create_app, db
 
-app = create_app('camputer.config.FlaskScriptConfig')
+app = create_app('camputer.config.DevelopmentConfig')
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)

c19af75aad3ae22a28f5eb76c2ef2a2d52ff94d0
Neil Bourgeois <neil.bourgeois@gmail.com>
2019-05-20 08:07:55 -0600

    handle text output from pi

--- scripts/core_temperature.py
+++ scripts/core_temperature.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import os, sys, getopt, sqlite3
+import os, re, sys, getopt, sqlite3
 from datetime import datetime
 
 SQL_INSERT_TEMPERATURE = "INSERT INTO sensor_reading (name, timestamp, value, uom) VALUES ('core', ?,?,'c')"
@@ -19,7 +19,11 @@ def main(argv):
 		elif opt in ("-d", "--dbfile"):
 			dbfile = arg
 
-	coreTemp = os.popen("vcgencmd measure_temp").readline()
+	#result = os.popen("vcgencmd measure_temp").readline()
+	result = "temp=21.3'C"
+	coreTemp = None
+	if result is not None and result != "":
+	    coreTemp = float(re.sub("\'C", "", re.sub("^temp=", "", result)))
 	if coreTemp is not None:
 		print('CoreTemp={0:0.1f}*'.format(coreTemp))
 		conn = sqlite3.connect(dbfile)
